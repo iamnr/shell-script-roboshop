a3ce3ca7194ede90e78ea25dfc1088f97a0984b2
iamnr <avuthunaveenreddy@gmail.com>
2023-10-29 15:26:43 +0530

    MongoDB

---
+++ catalogue.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+DATE=$(date +%F)
+LOGSDIR=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
+
+VALIDATE $? "Setting up NPM Source"
+
+yum install nodejs -y &>> $LOGFILE
+VALIDATE $? "Instalation of NODEJS"
+
+id roboshop &>> $LOGFILE
+
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "User ROBOSHOP creation"
+else
+    
+fi
+
+mkdir /app &>> $LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+VALIDATE $? "downloading catalogue artifact"
+
+cd /app &>> $LOGFILE
+VALIDATE $? "Moving into app directory"
+ 
+unzip /tmp/catalogue.zip &>> $LOGFILE
+VALIDATE $? "unzipping catalogue"
+
+npm install &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/shell-script-roboshop /etc/systemd/system/catalogue.service &>> $LOGFILE
+VALIDATE $? "copying catalogue.service"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "daemon reload"
+
+systemctl enable catalogue &>> $LOGFILE
+VALIDATE $? "Enabling Catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+VALIDATE $? "Starting Catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+VALIDATE $? "Copying mongo repo"
+
+yum install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing mongo client"
+
+mongo --host mongodb.devopspractice.site </app/schema/catalogue.js &>> $LOGFILE
+VALIDATE $? "loading catalogue data into mongodb"
+
+
+
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-VAR=$(id test)
-echo "$VAR"
